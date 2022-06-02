bc1e6fc622c888a4b204213280846cd8552326eb
asyanayana <71356587+asyanayana@users.noreply.github.com>
2022-06-02 21:26:33 +0700

    Project Keseluruhan

---
+++ app/src/main/java/com/pt2/iniresep/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -0,0 +1,74 @@
+package com.pt2.iniresep.ui.fragments.favorites
+
+import android.os.Bundle
+import android.os.Message
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.pt2.iniresep.R
+import com.pt2.iniresep.adapters.FavoriteRecipesAdapter
+import com.pt2.iniresep.databinding.FragmentFavoriteRecipesBinding
+import com.pt2.iniresep.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FavoriteRecipesFragment : Fragment() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter(requireActivity(), mainViewModel) }
+
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        binding.mAdapter = mAdapter
+
+        setHasOptionsMenu(true)
+
+        setupRecyclerView(binding.favoriteRecipesRecyclerView)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.favorite_recipes_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.deleteAll_favorite_recipes_menu) {
+            mainViewModel.deleteAllFavoriteRecipes()
+            showSnackBar()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun showSnackBar() {
+        Snackbar.make(
+            binding.root,
+            "All Recipes Removed",
+            Snackbar.LENGTH_SHORT
+        ).setAction("Okay"){}
+            .show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+        mAdapter.clearContextualActionMode()
+    }
+}
